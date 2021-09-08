34d645b56b10ebd0e4ffd115b8d5a9a740ff0fe9
Ilan Joselevich <personal@ilanjoselevich.com>
2021-09-08 19:58:16 +0300

    Use cache-search-path

--- flavors/lineageos/update.sh
+++ flavors/lineageos/update.sh
@@ -10,9 +10,9 @@ branch=$1
 
 args=(
     --ref-type branch
-    "https://github.com/ForkLineageOS/android"
+    --cache-search-path ../../
+    "https://github.com/LineageOS/android"
     "$branch"
-    */repo*.json
 )
 
 export TMPDIR=/tmp
