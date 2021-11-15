0148b0779206da52f8263d834c2bbb363b8d0879
Ilan Joselevich <personal@ilanjoselevich.com>
2021-11-15 01:13:01 +0000

    ForkLineageOS: Update

--- flavors/lineageos/default.nix
+++ flavors/lineageos/default.nix
@@ -78,7 +78,7 @@ mkIf (config.flavor == "lineageos")
 
   productNamePrefix = "lineage_"; # product names start with "lineage_"
 
-  buildDateTime = mkDefault 1636812764;
+  buildDateTime = mkDefault 1636938780;
 
   # LineageOS uses this by default. If your device supports it, I recommend using variant = "user"
   variant = mkDefault "userdebug";
